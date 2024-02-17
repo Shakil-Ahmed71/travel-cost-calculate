e533e60900bffeb26c4c7ecc9c0da47e496ae4b0
Shakil Ahmed <ahmedshakil00071@gmail.com>
2024-02-17 17:00:56 +0600

    added some style

--- script/style.js
+++ script/style.js
@@ -28,6 +28,16 @@ for (const btn of allBtn){
         selectedContainer.appendChild(li);
 
 
+        // alert a popup if the lowest budget
+        const budget = document.getElementById("budget").innerText;
+        const convertedBudget = parseInt(budget);
+        if(convertedBudget - parseInt(cartPrice) <0 ){
+            alert("Please erne more and come back");
+            return;
+        }
+        document.getElementById("budget").innerText = convertedBudget - parseInt(cartPrice);
+        
+
 
         // show the total cost undet the cart
         const totalCost = document.getElementById("total-cost").innerText;
@@ -35,12 +45,36 @@ for (const btn of allBtn){
         const sum = convertTotalCost + parseInt(cartPrice);
         document.getElementById("total-cost").innerText = sum;
 
-        
+        // show the grand total unde the cart
+
+        const grandTotal = document.getElementById("grand-total").innerText;
+        const convertGrandTotal = parseInt(grandTotal);
+        const sum2 = convertGrandTotal + parseInt(cartPrice);
+        document.getElementById("grand-total").innerText = sum2;
+
 
         setInnerText("card-count", count);
     });
 }
 
+
+function grandTotalElement(category) {
+    const totalCost = document.getElementById("total-cost").innerText;
+    const convertedTotalCost = parseInt(totalCost);
+
+    if (category === "bus") {
+        setInnerText("grand-total", convertedTotalCost + 100);
+    }
+    else if (category === "train"){
+        setInnerText("grand-total", convertedTotalCost - 200);
+    }
+    else if (category === "flight"){
+        setInnerText("grand-total", convertedTotalCost + 500);
+    }
+    else{
+        setInnerText("grand-total", convertedTotalCost);
+    }
+}
 
 
 
